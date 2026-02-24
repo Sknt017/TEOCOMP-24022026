b06a21f34460a4035af909f3c04a8bb3aed5afc4
dasan <dsanti1882@gmail.com>
2026-02-24 10:32:00 -0500

    feat: proyecto base

---
+++ src/regexvalidator/Modelo/dataManager.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package regexvalidator.Modelo;
+
+/**
+ *
+ * @author dasan
+ */
+public class dataManager {
+    public void llamadaDatos(float num, String sci, String nombre, String apellido, String email){
+        
+        
+    
+    }
+}
